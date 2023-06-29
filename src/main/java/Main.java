48a60c2afaf45fb35007b59ee708373d29d01656
SurferMax <max@surfer.media>
2023-06-29 21:55:37 +0300

    Task is done, captain. Where is my rum? Generics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,37 @@
 public class Main {
-    int result;
     public static void main(String[] args) {
-        CountingPeople count = new CountingPeople(); // Отдельный класс подсчета кол-ва людей;
-        int result = count.calculate(); // Считаем кол-во людей;
-        newGoodGenerator generator = new newGoodGenerator(); // Калькулятор из задания наывается в моем решении генератором;
-        do{
-            generator.generateNewGood(); // Создаем первый товар;
-        } while (generator.askingMoreGoods()); // Тут спрашиваем нужны ли еще товары;
-        // System.out.println("Добавили все товары, какие мы хотели");
-        generator.showTotalPrice();
-        generator.bill(result); // Метод проводит расчет по покупкам;
+        Pair pair = new Pair();
+        Sayable <Pair> tester = new Tester();
+        tester.theSameAs(pair);
     }
 }
+        class Pair {
+            public String StringSay;
+            public void saySomething() {
+                System.out.println("Метод saySomething сработал без generics");
+            }
+        }
+
+        class Tester implements Sayable<Pair> {
+                public void theSameAs(Pair object) {
+                System.out.println("В классе Тестер я говорю theSameAs сработал с generics: " + object);
+            }
+        }
+
+        interface Sayable<T> {
+            public void theSameAs(T object);
+        }
+
+
+
+
+
+//
+
+
+
+
+
 
 
 
